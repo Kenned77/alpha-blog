85e46ea42065f7ae59c2597abb7c307776872c43
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2021-11-18 15:53:25 -0800

    Create update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new()
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -25,5 +29,15 @@ class ArticlesController < ApplicationController
 
     # render plain: @article
     # render plain: @article.inspect
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] ="Article was Updated successfully!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
