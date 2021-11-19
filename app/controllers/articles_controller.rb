0ac554d270dac33f06facce2498fa390ee6f6465
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2021-11-18 16:12:02 -0800

    Add links to show and to delete a article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,4 +40,15 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] ="Article was Deleted successfully!"
+      redirect_to articles_path
+    else
+      redirect_to articles_path
+    end
+
+  end
 end
