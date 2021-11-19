a04bfa9faa5fa0efe87a6655df1afbccd6c3c6fc
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2021-11-18 16:57:37 -0800

    Comment out controller before refactoring the file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,3 +52,61 @@ class ArticlesController < ApplicationController
 
   end
 end
+
+
+
+
+# class ArticlesController < ApplicationController
+#   def show
+#     # byebug
+#     @article = Article.find(params[:id])
+#   end
+
+#   def index
+#     @articles = Article.all
+#   end
+
+#   def new
+#     @article = Article.new
+#   end
+
+#   def edit
+#     @article = Article.find(params[:id])
+#   end
+
+#   def create
+#     # render plain: params[:article]
+#     @article = Article.new(params.require(:article).permit(:title, :description))
+#     if @article.save
+#       flash[:notice] ="Article was created successfully!"
+#       redirect_to @article
+#     else
+#       render 'new'
+#     end
+#     # redirect_to article_path(@article)
+
+#     # render plain: @article
+#     # render plain: @article.inspect
+#   end
+
+#   def update
+#     @article = Article.find(params[:id])
+#     if @article.update(params.require(:article).permit(:title, :description))
+#       flash[:notice] ="Article was Updated successfully!"
+#       redirect_to @article
+#     else
+#       render 'edit'
+#     end
+#   end
+
+#   def destroy
+#     @article = Article.find(params[:id])
+#     if @article.destroy
+#       flash[:notice] ="Article was Deleted successfully!"
+#       redirect_to articles_path
+#     else
+#       redirect_to articles_path
+#     end
+
+#   end
+# end
